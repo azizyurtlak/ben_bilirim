6c80d64e5d385c93d5fa713fca29154fb4dfd978
Aziz Yurtlak <a.yurtlak@gmail.com>
2017-12-02 00:35:00 +0300

    jokers

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,5 +1,9 @@
 class ContestsController < ApplicationController
   def start
+    session[:half_joker] = true
+    session[:pass_joker] = true
+    session[:question_id] = nil
+
     if session[:user_id]
       session[:question_number] = 1
     else
@@ -8,13 +12,25 @@ class ContestsController < ApplicationController
   end
 
   def play
+    if params[:joker] == 'pass' && session[:pass_joker]
+      session[:question_number] = session[:question_number] + 1
+      session[:question_id] = nil
+      session[:pass_joker] = false
+    end
+
     @question_number = session[:question_number]
-    @question = Question.order("RANDOM()").first
+    @question = session[:question_id] ? Question.find(session[:question_id]) : Question.order("RANDOM()").first
     session[:question_id] = @question.id
+
+    if params[:joker] == 'half' && session[:half_joker]
+      @wrong_answers = ([0, 1, 2, 3] - [@question.correct_answer]).sample(2)
+      session[:half_joker] = false
+    end
   end
 
   def answer
     question = Question.find(session[:question_id])
+    session[:question_id] = nil
     question_number = session[:question_number]
     if question.correct_answer == params[:answer_index].to_i
       if question_number == 15
